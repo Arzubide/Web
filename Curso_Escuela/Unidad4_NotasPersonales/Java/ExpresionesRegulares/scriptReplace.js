423741f1a124e3cb4ee204f58b77f53a623b8034
Gael Arzubide Desktop <proyects.arzu@gmail.com>
2024-11-18 20:14:24 -0600

    se termina el codigo de replace y se cambia el nombre de regExp por regExpTest

--- Curso_Escuela/Unidad4_NotasPersonales/Java/ExpresionesRegulares/scriptReplace.js
+++ Curso_Escuela/Unidad4_NotasPersonales/Java/ExpresionesRegulares/scriptReplace.js
@@ -5,3 +5,12 @@ function palabra(){
     resultadoPatron = textoIngresado.replace(patron,cambiar);
     document.forms.datos.resultado.value = resultadoPatron;
 }
+
+
+function feliz(){
+    let PalabraUsu = document.getElementById("palabra").value;
+    var patron = /alegre|feliz|contento/g; //agregamos la bandera para que busque en toda la cadena
+    var cambio = "triste";
+    resultado = PalabraUsu.replace(patron,cambio);
+    document.getElementById("resultado").value = resultado;
+}

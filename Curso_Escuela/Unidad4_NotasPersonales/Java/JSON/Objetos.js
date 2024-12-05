0e5a69f5b9b7adfdd996e763089627f6f52d9919
Gael Arzubide <proyects.arzu@gmail.com>
2024-12-05 09:00:36 -0600

    Se termina de agregar la unidad 4 completa

---
+++ Curso_Escuela/Unidad4_NotasPersonales/Java/JSON/Objetos.js
@@ -0,0 +1,7 @@
+let prueba = {"nombre":"Gael","ApellidoPaterno": "Arzubide"};
+console.prueba.nombre;
+
+let tios = [
+    {"nombre":"Pancho", "apellido": "Santos"}, {"nombre": "Carlos", "apellido":"Davila"},{"nombre":"Irma","apellido":"Sanchez"}
+]
+
